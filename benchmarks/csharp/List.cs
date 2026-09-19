e5e7424d747f9ba7432abc9c0c8a57c9360f1153
Raul Santos <raulsntos@gmail.com>
2026-09-19 20:16:19 +0200

    Port PR 138 to enable C# benchmarks

--- benchmarks/csharp/List.cs
+++ benchmarks/csharp/List.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+using Godot.Collections;
 
 // Similar to GDScript Array benchmarks, but using C# List instead
 
@@ -6,115 +6,122 @@ public partial class List : Benchmark
 {
     private const int ITERATIONS = 2_000_000;
 
+    public void BenchmarkEmptyList()
+    {
+        Array<int> list = new Array<int>();
+        for (int i = 0; i < ITERATIONS; i++)
+        { list.IndexOf(0); }
+    }
+
     public void BenchmarkInt32List()
     {
-        List<int> list = new List<int>();
+        Array<int> list = new Array<int>();
 
-        for(int i = 0; i < ITERATIONS; i++)
+        for (int i = 0; i < ITERATIONS; i++)
         { list.Add(i); }
 
-        for(int i = 0; i < ITERATIONS; i++)
+        for (int i = 0; i < ITERATIONS; i++)
         { list[i] = 0; }
 
-        for(int i = 0; i < ITERATIONS; i++)
+        for (int i = 0; i < ITERATIONS; i++)
         { list.RemoveAt(list.Count - 1); }
     }
 
     public void BenchmarkInt64List()
     {
-        List<long> list = new List<long>();
+        Array<long> list = new Array<long>();
 
-        for(int i = 0; i < ITERATIONS; i++)
+        for (int i = 0; i < ITERATIONS; i++)
         { list.Add(i); }
 
-        for(int i = 0; i < ITERATIONS; i++)
+        for (int i = 0; i < ITERATIONS; i++)
         { list[i] = 0; }
 
-        for(int i = 0; i < ITERATIONS; i++)
+        for (int i = 0; i < ITERATIONS; i++)
         { list.RemoveAt(list.Count - 1); }
     }
 
     public void BenchmarkFloat32List()
     {
-        List<float> list = new List<float>();
+        Array<float> list = new Array<float>();
 
-        for(int i = 0; i < ITERATIONS; i++)
+        for (int i = 0; i < ITERATIONS; i++)
         { list.Add(i); }
 
-        for(int i = 0; i < ITERATIONS; i++)
+        for (int i = 0; i < ITERATIONS; i++)
         { list[i] = 0; }
 
-        for(int i = 0; i < ITERATIONS; i++)
+        for (int i = 0; i < ITERATIONS; i++)
         { list.RemoveAt(list.Count - 1); }
     }
 
     public void BenchmarkFloat64List()
     {
-        List<double> list = new List<double>();
+        Array<double> list = new Array<double>();
 
-        for(int i = 0; i < ITERATIONS; i++)
+        for (int i = 0; i < ITERATIONS; i++)
         { list.Add(i); }
 
-        for(int i = 0; i < ITERATIONS; i++)
+        for (int i = 0; i < ITERATIONS; i++)
         { list[i] = 0; }
 
-        for(int i = 0; i < ITERATIONS; i++)
+        for (int i = 0; i < ITERATIONS; i++)
         { list.RemoveAt(list.Count - 1); }
     }
 
     public void BenchmarkVector2List()
     {
-        List<Godot.Vector2> list = new List<Godot.Vector2>();
+        Array<Godot.Vector2> list = new Array<Godot.Vector2>();
 
-        for(int i = 0; i < ITERATIONS; i++)
+        for (int i = 0; i < ITERATIONS; i++)
         { list.Add(new Godot.Vector2(i, i)); }
 
-        for(int i = 0; i < ITERATIONS; i++)
+        for (int i = 0; i < ITERATIONS; i++)
         { list[i] = Godot.Vector2.Zero; }
 
-        for(int i = 0; i < ITERATIONS; i++)
+        for (int i = 0; i < ITERATIONS; i++)
         { list.RemoveAt(list.Count - 1); }
     }
 
     public void BenchmarkVector3List()
     {
-        List<Godot.Vector3> list = new List<Godot.Vector3>();
+        Array<Godot.Vector3> list = new Array<Godot.Vector3>();
 
-        for(int i = 0; i < ITERATIONS; i++)
+        for (int i = 0; i < ITERATIONS; i++)
         { list.Add(new Godot.Vector3(i, i, i)); }
 
-        for(int i = 0; i < ITERATIONS; i++)
+        for (int i = 0; i < ITERATIONS; i++)
         { list[i] = Godot.Vector3.Zero; }
 
-        for(int i = 0; i < ITERATIONS; i++)
+        for (int i = 0; i < ITERATIONS; i++)
         { list.RemoveAt(list.Count - 1); }
     }
 
     public void BenchmarkColorList()
     {
-        List<Godot.Color> list = new List<Godot.Color>();
+        Array<Godot.Color> list = new Array<Godot.Color>();
 
-        for(int i = 0; i < ITERATIONS; i++)
+        for (int i = 0; i < ITERATIONS; i++)
         { list.Add(new Godot.Color(i, i, i, 1.0f)); }
 
-        for(int i = 0; i < ITERATIONS; i++)
+        for (int i = 0; i < ITERATIONS; i++)
         { list[i] = Godot.Colors.Black; }
 
-        for(int i = 0; i < ITERATIONS; i++)
+        for (int i = 0; i < ITERATIONS; i++)
         { list.RemoveAt(list.Count - 1); }
     }
 
     public void BenchmarkStringList()
     {
-        List<string> list = new List<string>();
+        Array<string> list = new Array<string>();
 
-        for(int i = 0; i < ITERATIONS; i++)
+        for (int i = 0; i < ITERATIONS; i++)
         { list.Add("Godot " + i.ToString()); }
 
-        for(int i = 0; i < ITERATIONS; i++)
+        for (int i = 0; i < ITERATIONS; i++)
         { list[i] = ""; }
 
-        for(int i = 0; i < ITERATIONS; i++)
+        for (int i = 0; i < ITERATIONS; i++)
         { list.RemoveAt(list.Count - 1); }
     }
 }
--- main.gd
+++ main.gd
@@ -73,18 +73,37 @@ func _ready() -> void:
 		# from the interface after the fact.
 		item.set_checked(0, true)
 		if arg_include_benchmarks:
-			if not path.matchn(arg_include_benchmarks):
-				item.set_checked(0, false)
+			if arg_include_benchmarks.contains(";"):
+				print("Multiple include benchmarks specified, using semicolon as separator: %s" % arg_include_benchmarks)
+				var include_benchmarks := arg_include_benchmarks.split(";", false)
+				var matched := false
+				for include_benchmark in include_benchmarks:
+					if path.matchn(include_benchmark):
+						matched = true
+						break
+				if not matched:
+					item.set_checked(0, false)
+			else:
+				if not path.matchn(arg_include_benchmarks):
+					item.set_checked(0, false)
 		if arg_exclude_benchmarks:
-			if path.matchn(arg_exclude_benchmarks):
-				item.set_checked(0, false)
+			if arg_exclude_benchmarks.contains(";"):
+				print("Multiple exclude benchmarks specified, using semicolon as separator: %s" % arg_exclude_benchmarks)
+				var exclude_benchmarks := arg_exclude_benchmarks.split(";", false)
+				for exclude_benchmark in exclude_benchmarks:
+					if path.matchn(exclude_benchmark):
+						item.set_checked(0, false)
+						break
+			else:
+				if path.matchn(arg_exclude_benchmarks):
+					item.set_checked(0, false)
 
 		var results := Manager.get_test_result_as_dict(test_id)
 		var metric_names := ["render_cpu", "render_gpu", "idle", "physics", "time"]
 		for i in metric_names.size():
 			var metric = results.get(metric_names[i])
 			if metric:
-				item.set_text(i+1, "%s ms" % str(metric))
+				item.set_text(i + 1, "%s ms" % str(metric))
 				$CopyJSON.visible = true
 
 		items.append(item)
@@ -119,7 +138,7 @@ func _on_CopyJSON_pressed() -> void:
 
 
 func _on_Run_pressed() -> void:
-	var test_ids : Array[Manager.TestID] = []
+	var test_ids: Array[Manager.TestID] = []
 	for item in items:
 		if item.is_checked(0):
 			test_ids.push_back(item.get_meta("test_id"))
